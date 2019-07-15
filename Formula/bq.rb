2d6021536ab95dad564223eb768aa200d6b84e9d
Nikita Timofeev <stariy95@gmail.com>
2019-07-15 17:24:02 +0300

    experiments

--- Formula/bq.rb
+++ Formula/bq.rb
@@ -1,14 +1,16 @@
 class Bq < Formula
   desc "Bootique CLI tools"
   homepage "https://bootique.io"
-  url "https://github.com/bootique-tools/bq/releases/download/0.1/bq-0.1.zip"
-  sha256 "6785f0a463f5e9ef03289035a4b3cd41644631917957ad4600ab3d707d409b1f"
+  url "https://github.com/bootique-tools/bq/releases/download/0.1/bq-mac-0.1.zip"
+  sha256 "97597f25f610b7988d98922eabae58619c44fe4802f63825e85a2141f70f99b0"
 
   bottle :unneeded
 
   def install
+    chmod 0755, "bin/bq"
+
     libexec.install Dir["*"]
-    bin.install_symlink libexec/"bq"
+    bin.install_symlink libexec/"bin/bq"
   end
 
   test do
