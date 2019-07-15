72d4a48d7e67d370d64d94880071c17219eb9621
Nikita Timofeev <stariy95@gmail.com>
2019-07-15 17:09:36 +0300

    experiments

--- Formula/bq.rb
+++ Formula/bq.rb
@@ -4,6 +4,8 @@ class Bq < Formula
   url "https://github.com/bootique-tools/bq/releases/download/0.1/bq-0.1.zip"
   sha256 "6785f0a463f5e9ef03289035a4b3cd41644631917957ad4600ab3d707d409b1f"
 
+  bottle :unneeded
+
   def install
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bq"
